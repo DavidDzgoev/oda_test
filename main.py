f8160d6c575ccca6f7b974eff3895e5e62f30361
ddzgoev <david.dzgoev@mazars.ru>
2021-05-20 14:09:44 +0300

    added plotly dash and new route

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import yfinance as yf
+import plotly.express as px
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -8,11 +11,14 @@ app = FastAPI()
 async def root():
     return "Hello World! We are ODA!"
 
+
 @app.get("/{ticker}")
-async def read_item(ticker):
+async def get_data(ticker):
     return yf.download(tickers=ticker, period='max', interval='1d').to_json()
 
-@app.get("/")
-async def root():
-    return "Hello World!\nWe are ODA!"
 
+@app.get("/{ticker}/dash/{column}", response_class=HTMLResponse)
+async def get_dash(ticker, column):
+    df = yf.download(tickers=ticker, period='max', interval='1d')
+    fig = px.line(df, x=df.index, y=column, title=column)
+    return fig.to_html()
